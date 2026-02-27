95bf60501cfba80ebd854736ba296bebbb6ecf2e
Evan Nemetz <evannemetz@gmail.com>
2026-02-27 16:17:43 -0500

    Initial Commit

---
+++ Websight-2/Websight_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Websight_2App.swift
+//  Websight-2
+//
+//  Created by Evan Nemetz on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Websight_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Websight-2Tests/Websight_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Websight_2Tests.swift
+//  Websight-2Tests
+//
+//  Created by Evan Nemetz on 2/27/26.
+//
+
+import Testing
+@testable import Websight_2
+
+struct Websight_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
